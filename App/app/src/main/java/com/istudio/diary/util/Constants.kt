9f3808580f7c4d857f697e61e173e615803a2ab6
Devrath <devrath.dev595@gmail.com>
2023-02-16 22:29:34 +0530

    id's added

---
+++ App/app/src/main/java/com/istudio/diary/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.istudio.diary.util
+
+object Constants {
+    const val APP_ID ="diary-tszdx"
+    const val CLIENT_ID ="843098939374-alld2bg95cltklc1a4br968jn4hf025r.apps.googleusercontent.com"
+}
