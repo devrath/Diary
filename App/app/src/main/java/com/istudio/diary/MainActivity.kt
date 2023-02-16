42292aa47169185b1ba4931e11605d155356279f
Devrath <devrath.dev595@gmail.com>
2023-02-16 14:04:10 +0530

    image

--- App/app/src/main/java/com/istudio/diary/MainActivity.kt
+++ App/app/src/main/java/com/istudio/diary/MainActivity.kt
@@ -19,13 +19,7 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         setContent {
             DiaryAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                   // Greeting("Android")
-                }
+
             }
         }
     }
